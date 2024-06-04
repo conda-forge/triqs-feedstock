fc35e7fe25ccdde14e94dd7bc757d15e3b9e4119
Nils Wentzell <Wentzell@users.noreply.github.com>
2024-06-04 14:07:31 -0400

    Bump version to 3.3 (#59)
    * Bump version to 3.3
    * Add stdlib c build time requirement
    * Change PYTHON_EXECUTABLE to Python_ROOT_DIR in build.sh
    * Skip osx_64 builds
    * MNT: Re-rendered with conda-build 24.5.0, conda-smithy 3.36.0, and conda-forge-pinning 2024.05.27.20.59.24
    * Reduce number of cores when compiling
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>
    Co-authored-by: Thomas Hahn <thomas.hahn01@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,14 +17,14 @@ mpiexec="mpiexec --allow-run-as-root"
 
 export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
 cmake ${CMAKE_ARGS} \
-    -DPYTHON_EXECUTABLE=$PREFIX/bin/python \
+    -DPython_ROOT_DIR=$PREFIX \
     -DLIBCLANG_LOCATION=$PREFIX/lib \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DBLAS_LIBRARIES=$PREFIX/lib/libblas${SHLIB_EXT} \
     "-DLAPACK_LIBRARIES=$PREFIX/lib/liblapack${SHLIB_EXT};$PREFIX/lib/libblas${SHLIB_EXT}" \
     .. || cat CMakeFiles/CMakeError.log
 
-make -j${CPU_COUNT} VERBOSE=1
+make -j1 VERBOSE=1
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
   CTEST_OUTPUT_ON_FAILURE=1 ctest
